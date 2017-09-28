5216fb242a9864994eb34f8a8c442e28f67672aa
mmclkv <mmclkv@163.com>
2017-09-28 17:20:08 +0800

    roi_align_layer

--- include/caffe/mask_rcnn_layers.hpp
+++ include/caffe/mask_rcnn_layers.hpp
@@ -9,11 +9,19 @@
 #ifndef CAFFE_MASK_RCNN_LAYERS_HPP_
 #define CAFFE_MASK_RCNN_LAYERS_HPP_
 
+#include <cfloat>
+#include <iostream>
+#include <string>
+#include <utility>
 #include <vector>
+
+#include <algorithm>
+#include <stdlib.h>
 
 #include "caffe/blob.hpp"
 #include "caffe/common.hpp"
 #include "caffe/layer.hpp"
+#include "caffe/vision_layers.hpp"
 #include "caffe/proto/caffe.pb.h"
 
 #include "caffe/layers/loss_layer.hpp"
@@ -26,82 +34,82 @@ namespace caffe
 template <typename Dtype>
 class ROIAlignLayer : public Layer<Dtype>
 {
-  public:
-    explicit ROIAlignLayer(const LayerParameter &param)
-        : Layer<Dtype>(param) {}
-    virtual void LayerSetUp(const vector<Blob<Dtype> *> &bottom,
-                            const vector<Blob<Dtype> *> &top);
-    virtual void Reshape(const vector<Blob<Dtype> *> &bottom,
-                         const vector<Blob<Dtype> *> &top);
+public:
+  explicit ROIAlignLayer(const LayerParameter &param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype> *> &bottom,
+                          const vector<Blob<Dtype> *> &top);
+  virtual void Reshape(const vector<Blob<Dtype> *> &bottom,
+                       const vector<Blob<Dtype> *> &top);
 
-    virtual inline const char *type() const { return "RoIAlign"; }
+  virtual inline const char *type() const { return "RoIAlign"; }
 
-    virtual inline int MinBottomBlobs() const { return 2; }
-    virtual inline int MaxBottomBlobs() const { return 2; }
-    virtual inline int MinTopBlobs() const { return 1; }
-    virtual inline int MaxTopBlobs() const { return 1; }
+  virtual inline int MinBottomBlobs() const { return 2; }
+  virtual inline int MaxBottomBlobs() const { return 2; }
+  virtual inline int MinTopBlobs() const { return 1; }
+  virtual inline int MaxTopBlobs() const { return 1; }
 
-  protected:
-    virtual void Forward_cpu(const vector<Blob<Dtype> *> &bottom,
-                             const vector<Blob<Dtype> *> &top);
-    virtual void Forward_gpu(const vector<Blob<Dtype> *> &bottom,
-                             const vector<Blob<Dtype> *> &top);
-    virtual void Backward_cpu(const vector<Blob<Dtype> *> &top,
-                              const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
-    virtual void Backward_gpu(const vector<Blob<Dtype> *> &top,
-                              const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
+protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype> *> &bottom,
+                           const vector<Blob<Dtype> *> &top);
+  virtual void Forward_gpu(const vector<Blob<Dtype> *> &bottom,
+                           const vector<Blob<Dtype> *> &top);
+  virtual void Backward_cpu(const vector<Blob<Dtype> *> &top,
+                            const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype> *> &top,
+                            const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
 
-    int channels_;
-    int height_;
-    int width_;
-    int pooled_height_;
-    int pooled_width_;
-    Dtype spatial_scale_;
-    Blob<int> max_idx_;
+  int channels_;
+  int height_;
+  int width_;
+  int pooled_height_;
+  int pooled_width_;
+  Dtype spatial_scale_;
+  Blob<int> max_idx_;
 };
 
 template <typename Dtype>
 class SmoothL1LossLayer : public LossLayer<Dtype>
 {
-  public:
-    explicit SmoothL1LossLayer(const LayerParameter &param)
-        : LossLayer<Dtype>(param), diff_() {}
-    virtual void LayerSetUp(const vector<Blob<Dtype> *> &bottom,
-                            const vector<Blob<Dtype> *> &top);
-    virtual void Reshape(const vector<Blob<Dtype> *> &bottom,
-                         const vector<Blob<Dtype> *> &top);
+public:
+  explicit SmoothL1LossLayer(const LayerParameter &param)
+      : LossLayer<Dtype>(param), diff_() {}
+  virtual void LayerSetUp(const vector<Blob<Dtype> *> &bottom,
+                          const vector<Blob<Dtype> *> &top);
+  virtual void Reshape(const vector<Blob<Dtype> *> &bottom,
+                       const vector<Blob<Dtype> *> &top);
 
-    virtual inline const char *type() const { return "SmoothL1Loss"; }
+  virtual inline const char *type() const { return "SmoothL1Loss"; }
 
-    virtual inline int ExactNumBottomBlobs() const { return -1; }
-    virtual inline int MinBottomBlobs() const { return 2; }
-    virtual inline int MaxBottomBlobs() const { return 4; }
+  virtual inline int ExactNumBottomBlobs() const { return -1; }
+  virtual inline int MinBottomBlobs() const { return 2; }
+  virtual inline int MaxBottomBlobs() const { return 4; }
 
-    /**
+  /**
    * Unlike most loss layers, in the SmoothL1LossLayer we can backpropagate
    * to both inputs -- override to return true and always allow force_backward.
    */
-    virtual inline bool AllowForceBackward(const int bottom_index) const
-    {
-        return true;
-    }
+  virtual inline bool AllowForceBackward(const int bottom_index) const
+  {
+    return true;
+  }
 
-  protected:
-    virtual void Forward_cpu(const vector<Blob<Dtype> *> &bottom,
-                             const vector<Blob<Dtype> *> &top);
-    virtual void Forward_gpu(const vector<Blob<Dtype> *> &bottom,
-                             const vector<Blob<Dtype> *> &top);
+protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype> *> &bottom,
+                           const vector<Blob<Dtype> *> &top);
+  virtual void Forward_gpu(const vector<Blob<Dtype> *> &bottom,
+                           const vector<Blob<Dtype> *> &top);
 
-    virtual void Backward_cpu(const vector<Blob<Dtype> *> &top,
-                              const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
-    virtual void Backward_gpu(const vector<Blob<Dtype> *> &top,
-                              const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
+  virtual void Backward_cpu(const vector<Blob<Dtype> *> &top,
+                            const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype> *> &top,
+                            const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
 
-    Blob<Dtype> diff_;
-    Blob<Dtype> errors_;
-    Blob<Dtype> ones_;
-    bool has_weights_;
-    Dtype sigma2_;
+  Blob<Dtype> diff_;
+  Blob<Dtype> errors_;
+  Blob<Dtype> ones_;
+  bool has_weights_;
+  Dtype sigma2_;
 };
 
 } // namespace caffe
