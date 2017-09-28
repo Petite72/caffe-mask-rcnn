6d16b0bc5e056cf3a9bab0f3e4c813490281ed68
mmclkv <mmclkv@163.com>
2017-09-28 20:21:03 +0800

    modify

--- src/caffe/layers/smooth_L1_loss_layer.cpp
+++ src/caffe/layers/smooth_L1_loss_layer.cpp
@@ -5,7 +5,7 @@
 // Written by Ross Girshick
 // ------------------------------------------------------------------
 
-#include "caffe/fast_rcnn_layers.hpp"
+#include "caffe/mask_rcnn_layers.hpp"
 
 namespace caffe {
 
